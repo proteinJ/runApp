fdbe38d0d301f03337533165d42e47b9461a19e5
ProteinJ <jaehyun516@naver.com>
2026-01-22 14:42:47 +0900

    feat: add docker-compose and base source code

---
+++ src/main/java/com/running/runapp/RunappApplication.java
@@ -0,0 +1,15 @@
+package com.running.runapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class RunappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunappApplication.class, args);
+	}
+
+}
