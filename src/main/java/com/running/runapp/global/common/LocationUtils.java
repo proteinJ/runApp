eb61cb53a3c48d5bf6a07edb64197d5cc54fbec0
ProteinJ <jaehyun516@naver.com>
2026-02-10 18:31:26 +0900

    feat: LocationUtils 함수 최적화

--- src/main/java/com/running/runapp/global/common/LocationUtils.java
+++ src/main/java/com/running/runapp/global/common/LocationUtils.java
@@ -4,15 +4,14 @@ public class LocationUtils {
     private static final double EARTH_RADIUS = 6371000;
 
     // 하버사인 공식을 활용한 최단거리 구하기 메서드
-    public static double calculateDistance(double lat1, double lng1, double lat2, double lng2) {
+    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
         double dLat = Math.toRadians(lat2 - lat1);
-        double dLng = Math.toRadians(lng2 - lng1);
+        double dLon = Math.toRadians(lon2 - lon1);
 
         double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return EARTH_RADIUS * c;
+        return 2 * EARTH_RADIUS * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     }
 }
