00a98455c4cd73d216f74777bf8d4a38f8eefe1d
ProteinJ <jaehyun516@naver.com>
2026-03-29 17:26:51 +0900

    feat: 그룹러닝 builder를 객체지향형으로 Entity에서 처리할 수 있도록 toEntity 생성

--- src/main/java/com/running/runapp/domain/groupRunning/dto/GroupRequest.java
+++ src/main/java/com/running/runapp/domain/groupRunning/dto/GroupRequest.java
@@ -1,5 +1,9 @@
 package com.running.runapp.domain.groupRunning.dto;
 
+import com.running.runapp.domain.groupRunning.domain.GroupRunning;
+import com.running.runapp.domain.groupRunning.domain.GroupStatus;
+import com.running.runapp.domain.member.domain.Member;
+
 import java.time.LocalDateTime;
 
 public class GroupRequest {
@@ -13,7 +17,22 @@ public class GroupRequest {
         Integer distance,
         String location,
         String address
-    ) {}
+    ) {
+        public GroupRunning toEntity(Member host) {
+            return GroupRunning.builder()
+                    .title(this.title())
+                    .content(this.content())
+                    .maxParticipants(this.maxParticipants())
+                    .startTime(this.startTime())
+                    .endTime(this.endTime())
+                    .status(GroupStatus.RECRUITING)
+                    .host(host)
+                    .location(this.location())
+                    .address(this.address())
+                    .distance(this.distance())
+                    .build();
+        }
+    }
 
     public record UpdateExtraRequest(
        String title,
